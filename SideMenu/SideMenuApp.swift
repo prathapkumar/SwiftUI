feb1638b8bde73c7de26b9635125f77c38c1e0d4
Prathap <prathap.malempati@gmail.com>
2021-04-01 13:01:38 +0530

    Initial Commit

---
+++ SideMenu/SideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuApp.swift
+//  SideMenu
+//
+//  Created by Prathap on 01/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
